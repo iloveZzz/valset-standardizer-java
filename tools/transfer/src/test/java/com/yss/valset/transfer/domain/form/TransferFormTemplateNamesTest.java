95f70e868e73819353288d1ba768c92f88821df0
daomingzhu <zhudaoming@ysstech.com>
2026-04-25 02:05:57 +0800

    fixup!

--- tools/transfer/src/test/java/com/yss/valset/transfer/domain/form/TransferFormTemplateNamesTest.java
+++ tools/transfer/src/test/java/com/yss/valset/transfer/domain/form/TransferFormTemplateNamesTest.java
@@ -3,8 +3,6 @@ package com.yss.valset.transfer.domain.form;
 import com.yss.valset.transfer.domain.model.TargetType;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -19,19 +17,21 @@ class TransferFormTemplateNamesTest {
     }
 
     @Test
-    void should_use_directory_field_for_filesys_target_template() {
+    void should_use_target_path_template_for_filesys_target_template() {
         TransferTargetFilesysFormTemplate template = new TransferTargetFilesysFormTemplate();
 
+        assertThat(template.buildForm().getSchema().getProperties())
+                .containsKey("layout");
         @SuppressWarnings("unchecked")
-        Map<String, Object> layoutNode = template.buildForm().getSchema().getProperties().get("layout").toMap();
+        java.util.Map<String, Object> layoutNode = template.buildForm().getSchema().getProperties().get("layout").toMap();
         @SuppressWarnings("unchecked")
-        Map<String, Object> gridNode = (Map<String, Object>) ((Map<String, Object>) layoutNode.get("properties")).get("grid");
+        java.util.Map<String, Object> gridNode = (java.util.Map<String, Object>) ((java.util.Map<String, Object>) layoutNode.get("properties")).get("grid");
         @SuppressWarnings("unchecked")
-        Map<String, Object> gridProperties = (Map<String, Object>) gridNode.get("properties");
+        java.util.Map<String, Object> gridProperties = (java.util.Map<String, Object>) gridNode.get("properties");
 
-        assertThat(gridProperties).containsKey("directory");
+        assertThat(gridProperties).containsKey("targetPathTemplate");
         @SuppressWarnings("unchecked")
-        Map<String, Object> directoryNode = (Map<String, Object>) gridProperties.get("directory");
-        assertThat(directoryNode).containsEntry("title", "目录路径");
+        java.util.Map<String, Object> pathNode = (java.util.Map<String, Object>) gridProperties.get("targetPathTemplate");
+        assertThat(pathNode).containsEntry("title", "目标路径模板");
     }
 }
